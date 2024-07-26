6342b706a26ff34fecfc4ca8936c3913f5d9f92e
der-himmel <105467009+der-himmel@users.noreply.github.com>
2024-07-26 15:21:02 +0300

    Create gen.py

---
+++ proj/pack/certificates/gen.py
@@ -0,0 +1,48 @@
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+from pydantic import BaseModel, FilePath
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    prefix="/gen-keys",
+    tags=["Generate keys"]
+)
+
+class AuthJWT(BaseModel):
+    PRIVATE_KEY_PATH: FilePath = ".../proj/pack/certificates/private.pem"
+    PUBLIC_KEY_PATH: FilePath = ".../proj/pack/certificates/public.pem"
+
+keys = AuthJWT()
+
+key = rsa.generate_private_key(
+    backend=crypto_default_backend(),
+    public_exponent=65537,
+    key_size=2048
+)
+
+
+@router.get("/generatekeys")
+def keys_generator():
+    open(keys.PUBLIC_KEY_PATH, 'wb').close()
+    open(keys.PRIVATE_KEY_PATH, 'wb').close()
+
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PrivateFormat.PKCS8,
+        crypto_serialization.NoEncryption()
+    )
+
+    public_key = key.public_key().public_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    with open(keys.PUBLIC_KEY_PATH, 'wb') as pub_file:
+        pub_file.write(public_key)
+
+    with open(keys.PRIVATE_KEY_PATH, 'wb') as prv_file:
+        prv_file.write(private_key)
+
+    print("KEYS GENERATED")
